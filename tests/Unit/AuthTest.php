14d666c218ac26cd26f506e885afd80b38ab4831
Elliot <gt520490@fmail.com>
2020-04-15 21:48:54 +0800

    0415

--- tests/Unit/AuthTest.php
+++ tests/Unit/AuthTest.php
@@ -4,15 +4,7 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+class AutheTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $this->assertTrue(true);
-    }
+   
 }
